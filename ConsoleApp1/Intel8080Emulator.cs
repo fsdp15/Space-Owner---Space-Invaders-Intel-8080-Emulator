167ea895cceffca33b16b19303419cd2473bd73f
Felipe Prado <felipe.dprado@live.com>
2023-01-10 00:41:08 +0000

    Continuing emulation.

--- ConsoleApp1/Intel8080Emulator.cs
+++ ConsoleApp1/Intel8080Emulator.cs
@@ -9,11 +9,17 @@ namespace Intel8080Emulator
 {
     internal unsafe class Intel8080Emulator
     {
+        public static byte Parity(ushort parity)
+        {
+            return (byte) parity;
+        }
+
         public int Emulate8080Op(Registers registers)
         {
             byte* opcode = &registers.Memory[registers.Pc];
+            ushort answer;
 
-            switch (opcode) // I can implement this with a dictionary that poins to a... method? (delegate)
+            switch (*opcode) // I can implement this with a dictionary that points to a... method? (delegate)
             {
                 case 0x00: // NOP
                     break;
@@ -30,6 +36,33 @@ namespace Intel8080Emulator
                     registers.B = registers.C;
                     break;
 
+                case 0x80: // 	ADD B
+                    answer = (ushort)((ushort)registers.A + (ushort)registers.B); // Higher precision to capture carry out
+                    if ((answer & (ushort)0xff) == 0) registers.Flags.Z = 1; else registers.Flags.Z = 0; // Zero Flag
+                    if ((answer & (ushort)0x80) >= 0x80) registers.Flags.S = 1; else registers.Flags.S = 0; // Sign Flag
+                    if ((answer > (ushort)0xff)) registers.Flags.Cy = 1; else registers.Flags.Cy = 0; // Carry Flag
+                    registers.Flags.P = Intel8080Emulator.Parity(answer);
+                    registers.A = (byte)(answer & (ushort) 0xff); // Returning to 8 bits
+                    break;
+
+                case 0xC6: // 	ADI D8
+                    answer = (ushort)((ushort)registers.A + (ushort)opcode[1]); // Higher precision to capture carry out
+                    if ((answer & (ushort)0xff) == 0) registers.Flags.Z = 1; else registers.Flags.Z = 0; // Zero Flag
+                    if ((answer & (ushort)0x80) >= 0x80) registers.Flags.S = 1; else registers.Flags.S = 0; // Sign Flag
+                    if ((answer > (ushort)0xff)) registers.Flags.Cy = 1; else registers.Flags.Cy = 0; // Carry Flag
+                    registers.Flags.P = Intel8080Emulator.Parity(answer);
+                    registers.A = (byte)(answer & (ushort)0xff); // Returning to 8 bits
+                    break;
+
+                case 0x86: // ADD M -> | A <- A + (HL)
+                    ushort offset = (ushort)((ushort)(registers.H << 8) | (ushort)registers.L);
+                    answer = (ushort)((ushort)registers.A + (ushort)registers.Memory[offset]); // Higher precision to capture carry out
+                    if ((answer & (ushort)0xff) == 0) registers.Flags.Z = 1; else registers.Flags.Z = 0; // Zero Flag
+                    if ((answer & (ushort)0x80) >= 0x80) registers.Flags.S = 1; else registers.Flags.S = 0; // Sign Flag
+                    if ((answer > (ushort)0xff)) registers.Flags.Cy = 1; else registers.Flags.Cy = 0; // Carry Flag
+                    registers.Flags.P = Intel8080Emulator.Parity(answer);
+                    registers.A = (byte)(answer & (ushort)0xff); // Returning to 8 bits
+                    break;
 
             }
 
